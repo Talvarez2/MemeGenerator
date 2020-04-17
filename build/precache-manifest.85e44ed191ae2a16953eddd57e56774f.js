1c3f83314d48c2dbef71fad8e1b6f1fc3393e9b9
Tomas Alvarez <talvarez2@uc.cl>
2020-04-17 17:15:55 -0400

    deploying

---
+++ build/precache-manifest.85e44ed191ae2a16953eddd57e56774f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2311d6e4717a91ee0cf5aa2080072911",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e74a09fc3a81c3fdf5c",
+    "url": "/static/js/2.352b05e8.chunk.js"
+  },
+  {
+    "revision": "2f33cf77e2cc3763164007ca9cab1c5e",
+    "url": "/static/js/2.352b05e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23214c6ee704c0b61fd8",
+    "url": "/static/js/main.435c9912.chunk.js"
+  },
+  {
+    "revision": "63b9dc02b0071dc7aebf",
+    "url": "/static/js/runtime-main.7d20a365.js"
+  }
+]);
