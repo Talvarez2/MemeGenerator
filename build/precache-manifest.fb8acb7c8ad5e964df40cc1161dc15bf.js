b8f4664ccba299af6b10cc23760725f763aac781
Tomas Alvarez <talvarez2@uc.cl>
2020-04-17 17:22:20 -0400

    fixing package

---
+++ build/precache-manifest.fb8acb7c8ad5e964df40cc1161dc15bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb21e7a86950d75d0c68e75da30e3ab",
+    "url": "/MemeGenerator/index.html"
+  },
+  {
+    "revision": "5e74a09fc3a81c3fdf5c",
+    "url": "/MemeGenerator/static/js/2.352b05e8.chunk.js"
+  },
+  {
+    "revision": "2f33cf77e2cc3763164007ca9cab1c5e",
+    "url": "/MemeGenerator/static/js/2.352b05e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23214c6ee704c0b61fd8",
+    "url": "/MemeGenerator/static/js/main.435c9912.chunk.js"
+  },
+  {
+    "revision": "5e04ca1d6480df2603d4",
+    "url": "/MemeGenerator/static/js/runtime-main.befa1083.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.85e44ed191ae2a16953eddd57e56774f.js"
+  "/MemeGenerator/precache-manifest.fb8acb7c8ad5e964df40cc1161dc15bf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MemeGenerator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
